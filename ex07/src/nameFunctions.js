47d8d72185b5fb041b9b5e6d6ffe648dcb55e709
basslines <admir.osmanovic@gmail.com>
2021-03-23 22:44:30 +0100

    dodao index.js fajlove

---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,11 @@
+const firstName = (firstName) => {
+  return firstName.toUpperCase();
+};
+const lastName = (lastName) => {
+  return lastName.toLowerCase();
+};
+exports.firstName = firstName;
+exports.lastName = lastName;
+
+console.log(firstName("your name"));
+console.log(lastName("MY NAME"));
